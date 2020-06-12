1fea69698ac5c7b09e3737785e291f234e86082d
Angus6930 <66456154+Angus6930@users.noreply.github.com>
2020-06-12 13:08:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function generateRandomNumber() {
   $('#output').html('')
-  const max = 26
+  const max = 19
   const min = 1
   const numbers = []
   let randomNum = -1
-  while (numbers.length < 25) {
+  while (numbers.length < 18) {
     randomNum = Math.floor(Math.random() * (max - min) + min)
     if (!numbers.includes(randomNum)) {
       numbers.push(randomNum)
